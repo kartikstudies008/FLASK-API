66c16f81fd8878ec75fd67d83e5ef8c91885e666
Kartik <kartikstudies008@gmail.com>
2026-03-22 16:24:04 +0530

    changes

---
+++ labmst/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, session, redirect, url_for
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+# secret key required for sessions
+app.secret_key = "kartik_secret"
+
+# session timeout = 30 seconds
+app.permanent_session_lifetime = timedelta(seconds=30)
+
+
+@app.route("/")
+def start_session():
+
+    session.permanent = True
+    session["start_time"] = datetime.now().strftime("%H:%M:%S")
+
+    return f"""
+    <h2>Session Started</h2>
+    Start Time: {session['start_time']}<br>
+    Refresh within 30 seconds or session will expire.
+    <br><br>
+    <a href='/check'>Check Session</a>
+    """
+
+
+@app.route("/check")
+def check_session():
+
+    if "start_time" in session:
+        return f"""
+        <h2>Session Active</h2>
+        Start Time: {session['start_time']}
+        """
+
+    else:
+        return "<h2>Session Expired</h2>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
